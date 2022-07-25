13ffeb25bddc1f88b813e857abbfd569bdc8084c
AlexJjunio <alex_jjunio@outlook.com>
2022-07-25 09:07:19 -0300

    add ex.js

--- aula011/ex008.js
+++ aula011/ex008.js
@@ -1,1 +1,6 @@
-console.logo('O console funcionou corretamente!')
+var vel = 60
+console.log (`À velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { //Condição simples! (Não possui o "else")
+    console.log (`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
