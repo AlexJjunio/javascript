5b0eac254aed3c55fff8facc7def445b27ba7e38
AlexJjunio <alex_jjunio@outlook.com>
2022-07-30 12:01:56 -0300

    js

--- aulas/aula011/ex008.js
+++ aulas/aula011/ex008.js
@@ -1,6 +1,6 @@
-var vel = 60
-console.log (`À velocidade do seu carro é ${vel}Km/h`)
+var vel = 61
+console.log (`A velocidade do seu carro é ${vel}Km/h`)
 if (vel > 60) { //Condição simples! (Não possui o "else")
     console.log (`Voce ultrapassou a velocidade permitida. MULTADO!`)
 }
-console.log(`Dirija sempre usando cinto de segurança!`)
+console.log('Dirija sempre usando cinto de segurança!')
