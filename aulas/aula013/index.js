6ff791e322f54d3ebb3fecd4261c8ba80d9bda2e
AlexJjunio <alex_jjunio@outlook.com>
2022-08-02 12:08:43 -0300

    add

---
+++ aulas/aula013/index.js
@@ -0,0 +1,26 @@
+
+
+var c = 1
+do { //do "faça isso tudo..."
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+} while (c <= 6) //enquanto meu contador for menor ou igual a seis.
+
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+}*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+// As 3 funções tem praticamente a mesma funcionalidade. 
