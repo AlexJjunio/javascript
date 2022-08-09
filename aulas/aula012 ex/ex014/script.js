2c2b84d3d770cc51d61844356e31150f9d957fb8
AlexJjunio <alex_jjunio@outlook.com>
2022-08-09 12:12:04 -0300

    add ex 12 e 17

---
+++ aulas/aula012 ex/ex014/script.js
@@ -0,0 +1,18 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    // var hora = 19
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/foto-manha.jpg'
+        document.body.style.background = '#CEA1B3'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'imagens/foto-tarde.jpg'
+        document.body.style.background = '#B4B4A5'
+    } else {
+        img.src = 'imagens/foto-noite.jpg'
+        document.body.style.background = '#151E17'
+    }
+} 
