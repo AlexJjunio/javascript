4c8a4d0b76edfcece987dff66b36ba98475da4d4
AlexJjunio <alex_jjunio@outlook.com>
2022-08-11 04:17:36 -0300

    verificador de idade

--- aulas/aula012 ex/ex014/script.js
+++ aulas/aula012 ex/ex014/script.js
@@ -3,6 +3,7 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    var min = data.getMinutes()
     // var hora = 19
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
